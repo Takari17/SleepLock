9fd40b14fe3f3fafc51df38d54fd8c8212020c43
Takari Harrison <takariharrison@gmail.com>
2019-05-31 19:32:38 -0400

    Fix notification not showing on API's 26 and above
    *Added a notification channel and requested the foreground
    service permission to fix the foreground notification not appearing
    on API's 26 and above.

--- app/src/main/java/com/example/sleeplock/Application.kt
+++ app/src/main/java/com/example/sleeplock/Application.kt
@@ -1,21 +1,42 @@
 package com.example.sleeplock
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.example.sleeplock.injection.ApplicationComponent
 import com.example.sleeplock.injection.DaggerApplicationComponent
+import com.example.sleeplock.utils.CHANNEL_ID
 
 /*
 Exposes Dagger component globally for classes I do not own (e.g Activities & Services)
  */
 class Application : Application() {
 
-    companion object{
+    companion object {
         lateinit var applicationComponent: ApplicationComponent
     }
 
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
         applicationComponent = DaggerApplicationComponent.factory()
             .create(applicationContext)
+    }
+
+    //Notification Channel for API's above 26
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                getString(R.string.channel_name),
+                NotificationManager.IMPORTANCE_LOW
+            ).apply { description = getString(R.string.channel_description) }
+
+            val notificationManager = (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
