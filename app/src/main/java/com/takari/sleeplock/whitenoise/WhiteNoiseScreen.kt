fd7388917b3a8640f1067ca486f379c0c9c56df9
Takari <takariharrison@gmail.com>
2023-06-16 08:52:43 -0400

    Implement alpha animations when media is playing

--- app/src/main/java/com/takari/sleeplock/whitenoise/WhiteNoiseScreen.kt
+++ app/src/main/java/com/takari/sleeplock/whitenoise/WhiteNoiseScreen.kt
@@ -1,6 +1,10 @@
 package com.takari.sleeplock.whitenoise
 
 import SleepLockTimeSelectionDialog
+import android.util.Log
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -16,6 +20,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,14 +29,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.takari.sleeplock.R
 import com.takari.sleeplock.whitenoise.data.WhiteNoise
 import com.takari.sleeplock.whitenoise.service.WhiteNoiseService
-import com.takari.sleeplock.whitenoise.ui.WhiteNoiseViewModel
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -57,36 +64,55 @@ fun WhiteNoiseScreen(viewModel: WhiteNoiseViewModel) {
                         )
                     }
 
-                WhiteNoiseView(whiteNoise = item, imageModifier = imageModifier)
+                WhiteNoiseItem(whiteNoise = item, imageModifier = imageModifier)
             }
         }
 
-        Text(
+        FadingText(
+            modifier = Modifier.padding(start = 8.dp, top = 24.dp),
             text = "Pick a Sound!",
+            fadingCondition = whiteNoiseUiState.mediaIsPlaying,
             color = Color.White,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 8.dp, top = 8.dp)
         )
 
         Image(
-            painter = painterResource(id = R.drawable.transparant_play_icon),
+            painter = painterResource(id = whiteNoiseUiState.mediaOption),
             contentDescription = null,
             modifier = Modifier
                 .align(Alignment.Center)
                 .size(100.dp)
         )
+
+        FadingButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 64.dp),
+            fadingCondition = whiteNoiseUiState.mediaIsPlaying,
+            onClick = { viewModel.resetState() },
+        )
+
+        FadingText(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 64.dp),
+            text = whiteNoiseUiState.timer,
+            color = Color.White,
+            fontSize = 64.sp,
+            fadingCondition = !whiteNoiseUiState.mediaIsPlaying,
+        )
     }
 
-    OpenTimeOptionsDialog(
+    SelectTimeDialog(
         showTimePicker = whiteNoiseUiState.showTimePicker,
         onCancel = { viewModel.closeDialog() },
-        onTimeSelected = { viewModel.closeDialog() }
+        onTimeSelected = { millis: Long -> viewModel.onUserSelectedTimeFromDialog(millis) }
     )
 }
 
 @Composable
-fun WhiteNoiseView(whiteNoise: WhiteNoise, imageModifier: Modifier) {
+fun WhiteNoiseItem(whiteNoise: WhiteNoise, imageModifier: Modifier) {
     Box {
         AsyncKensBurnImage(
             imageID = whiteNoise.image(),
@@ -118,7 +144,7 @@ fun WhiteNoiseView(whiteNoise: WhiteNoise, imageModifier: Modifier) {
 }
 
 @Composable
-fun OpenTimeOptionsDialog(
+fun SelectTimeDialog(
     showTimePicker: Boolean,
     onCancel: () -> Unit = {},
     onTimeSelected: (Long) -> Unit = { }
@@ -127,5 +153,56 @@ fun OpenTimeOptionsDialog(
         showTimePicker,
         onCancel = { onCancel() },
         onTimeSelected = { onTimeSelected(it) }
+    )
+}
+
+
+@Preview
+@Composable
+fun FadingButton(
+    modifier: Modifier = Modifier,
+    fadingCondition: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+
+    val visibility: Float by animateFloatAsState(
+        targetValue = if (fadingCondition) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000),
+    )
+
+    Button(
+        onClick = { onClick() },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+        border = BorderStroke(1.dp, Color.White),
+        modifier = modifier
+            .width(150.dp)
+            .graphicsLayer(alpha = visibility),
+    ) {
+        Text(text = "Reset", color = Color.White)
+    }
+}
+
+@Composable
+fun FadingText(
+    modifier: Modifier = Modifier,
+    text: String = "This is a sentence.",
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fadingCondition: Boolean = true,
+) {
+
+    val visibility: Float by animateFloatAsState(
+        targetValue = if (fadingCondition) 0f else 1f,
+        animationSpec = tween(durationMillis = 1000),
+    )
+
+    Text(
+        text = text,
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        maxLines = 1,
+        modifier = modifier.graphicsLayer(alpha = visibility)
     )
 }
--- app/src/main/java/com/takari/sleeplock/whitenoise/WhiteNoiseViewModel.kt
+++ app/src/main/java/com/takari/sleeplock/whitenoise/WhiteNoiseViewModel.kt
@@ -1,9 +1,11 @@
-package com.takari.sleeplock.whitenoise.ui
+package com.takari.sleeplock.whitenoise
 
 import androidx.lifecycle.ViewModel
-import com.takari.sleeplock.whitenoise.WhiteNoiseUiState
+import com.takari.sleeplock.R
 import com.takari.sleeplock.whitenoise.data.WhiteNoise
 import com.takari.sleeplock.whitenoise.data.WhiteNoiseOptions
+import com.takari.sleeplock.whitenoise.data.sounds.Rain
+import com.takari.sleeplock.whitenoise.ui.WhiteNoiseViewCommands
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,6 +19,7 @@ class WhiteNoiseViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(WhiteNoiseUiState())
     val uiState: StateFlow<WhiteNoiseUiState> = _uiState.asStateFlow()
+
 
     fun onAdapterClick(
         clickedWhiteNoise: WhiteNoise,
@@ -33,6 +36,24 @@ class WhiteNoiseViewModel : ViewModel() {
         }
     }
 
+    fun onUserSelectedTimeFromDialog(millis: Long) {
+        if (millis != 0L) {
+            _uiState.value = _uiState.value.copy(
+                showTimePicker = false,
+                mediaIsPlaying = true,
+                mediaOption = R.drawable.transparant_pause_icon
+            )
+
+            viewCommand(WhiteNoiseViewCommands.StartAnimation)
+            viewCommand(
+                WhiteNoiseViewCommands.StartAndBindToService(
+                    millis,
+                    clickedWhiteNoise ?: Rain()
+                )
+            )
+        }
+    }
+
     fun getWhiteNoiseOptions(): List<WhiteNoise> {
         //todo look at that google project, look at how they handle the data layer when it's not needed
         return WhiteNoiseOptions.get
@@ -40,5 +61,10 @@ class WhiteNoiseViewModel : ViewModel() {
 
     fun closeDialog() {
         _uiState.value = _uiState.value.copy(showTimePicker = false)
+    }
+
+    fun resetState() {
+        clickedWhiteNoise = null
+        _uiState.value = WhiteNoiseUiState()
     }
 }
