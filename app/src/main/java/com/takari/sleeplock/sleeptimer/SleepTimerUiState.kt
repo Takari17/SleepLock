e066bb568fdfaf0d3b0eeecc50a5d899b8f1f292
Takari <takariharrison@gmail.com>
2023-06-18 16:54:44 -0400

    Add TimeSelector dialog within SleepTimer's screen

---
+++ app/src/main/java/com/takari/sleeplock/sleeptimer/SleepTimerUiState.kt
@@ -0,0 +1,8 @@
+package com.takari.sleeplock.sleeptimer
+
+data class SleepTimerUiState(
+    val showTimePickerDialog: Boolean = false,
+    val timerServiceIsRunning: Boolean = false,
+    val elapseTime: String = "00:00",
+    val isTimerRunning: Boolean = false, //todo set default value to  SleepTimerService.timerIsRunning()
+)
