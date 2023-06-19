055922dff4378c2d06556631b2b418319e138d15
Takari <takariharrison@gmail.com>
2023-06-19 16:14:40 -0400

    Fix device not sleeping in SleepTimer when the timer reached 0

--- app/src/main/java/com/takari/sleeplock/sleeptimer/admin/PermissionResult.kt
+++
@@ -1,9 +0,0 @@
-package com.takari.sleeplock.sleeptimer.admin
-
-/**
- * Result code values returned in onActivityResult when the user is asked to
- * enabled admin permissions.
- */
-enum class PermissionResult(val code: Int) {
-    UserCanceled(0), UserConfirmed(-1)
-}
