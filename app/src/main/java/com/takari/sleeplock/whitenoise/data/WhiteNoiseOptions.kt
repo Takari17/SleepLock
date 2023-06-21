0e2874d45b8dc7dd498a9ad4ba9618f0a6d05489
Takari <takariharrison@gmail.com>
2023-06-21 14:44:10 -0400

    Add DeepSea WhiteNoise

--- app/src/main/java/com/takari/sleeplock/whitenoise/data/WhiteNoiseOptions.kt
+++ app/src/main/java/com/takari/sleeplock/whitenoise/data/WhiteNoiseOptions.kt
@@ -6,14 +6,14 @@ import com.takari.sleeplock.whitenoise.data.sounds.*
  * Defines the white noises and viewing order of them used throughout this module. The data in
  * this module is static, so there's no need for a data base.
  *
- * To add a new sound just create a new Impl of WhiteNoise and add it to this list in the
- * order you want.
+ * To add a new sound just create a new implementation of WhiteNoise and add it to this
+ * list in the order you want.
  */
 object WhiteNoiseOptions {
     val get = listOf(
         Rain(),
         OceanShore(),
-//        DeepSea(),
+        DeepSea(),
         Sunrise(),
         Nightfall(),
         Helicopter(),
